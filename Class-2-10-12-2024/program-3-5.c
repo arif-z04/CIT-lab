2189c5349e7e72d1e45479b9c6cb3935d6a2ed76
S.M.Arifuzzaman <smarifuzzaman123@gmail.com>
2024-12-10 12:59:49 +0600

    added files

--- Class-2-10-12-2024/program-3-5.c
+++ Class-2-10-12-2024/program-3-5.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef char Item;
 typedef int Prices;
 
-int main(){
-    Item name[5][2];
+int main() {
+    Item name[2][50]; 
     Prices item[2];
 
-    for(int i = 0; i<2; i++){
+    for (int i = 0; i < 2; i++) {
         printf("Item name: ");
-                
+        fgets(name[i], sizeof(name[i]), stdin);
+
+        name[i][strcspn(name[i], "\n")] = '\0';
+
+        printf("Prices: ");
+        scanf("%d", &item[i]);
+        while (getchar() != '\n');  
     }
-     
+
+    printf("\n***LIST OF PRICES***\n");
+    printf("Item\tPrices\n");
+    
+    for (int i = 0; i < 2; i++) {
+        printf("%s\tRs %d\n", name[i], item[i]);
+    }
+
+    return 0;
 }
