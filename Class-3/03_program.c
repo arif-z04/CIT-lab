148a66ebd8dc8ee5497dd798f683516971361cd6
S.M.Arifuzzaman <smarifuzzaman123@gmail.com>
2025-01-07 15:11:42 +0600

    added new files

---
+++ Class-3/03_program.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int weight;
+    scanf("%d", &weight);
+
+    if(weight > 150 && weight < 170){
+        printf("Normal.\n");
+    } else if (weight < 150){
+        printf("Underweight.\n");
+    } else {
+        printf("Overweight.\n");
+    }
+    
+    return 0;
+}
